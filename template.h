037d1d0b77fbe783de3ded4c6356f028dd1d7684
Wankai Zhang <wankaizhang@gmail.com>
2017-02-06 13:29:47 +0800

    Update template.h

--- template.h
+++ template.h
@@ -4,6 +4,51 @@
 
 namespace hte {
 
+class NameValue
+{
+ public:
+  static NameValue Build(const std::string& name, const std::string& value)
+  {
+    NameValue tmp;
+    tmp.set_name(name);
+    tmp.set_value(value);
+  }
+ 
+  inline const std::string& name()
+  {
+    return name_;
+  }
+ 
+  inline void set_name(const std::string& name)
+  {
+    name_ = name;
+  }
+ 
+  inline const std::string& value()
+  {
+    return value_;
+  }
+ 
+  inline void set_value(const std::string& value)
+  {
+    value_ = value;
+  }
+ 
+ private:
+  string name_;
+  string value_;
+};
+
+class Section
+{
+ public:
+  
+ private:
+  std::string pattern_;
+  std::regex pattern_regex_;
+  std::vector<NameValue> name_values_;
+};
+
 class Template
 {
  public:

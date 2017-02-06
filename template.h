b674954a96df10df62ce45521b582bd25154f804
Wankai Zhang <wankaizhang@gmail.com>
2017-02-06 13:22:07 +0800

    Update template.h

--- template.h
+++ template.h
@@ -7,6 +7,15 @@ namespace hte {
 class Template
 {
  public:
+  inline const std::string& site()
+  {
+    return site_;
+  }
+ 
+  inline const std::vector<Section>& sections()
+  {
+    return sections_;
+  }
  private:
   std::string site_;
   std::vector<Section> sections_;

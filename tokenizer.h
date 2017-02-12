232fe85b6a3fef8bbd01e4d409653b60ffee6f9a
Wankai Zhang <wankaizhang@gmail.com>
2017-02-12 15:33:10 +0800

    Update tokenizer.h

--- tokenizer.h
+++ tokenizer.h
@@ -2,6 +2,28 @@
 
 namespace hte {
 
+enum TokenType
+{
+  kOpenedTag = 1,
+  kClosedTag = 2,
+};
+
+class Token {
+ public:
+  TokenType type() const
+  {
+    return type_;
+  }
+ 
+  const std::string& value() const
+  {
+    return value_;
+  }
+ private:
+  std::string value_;
+  TokenType type_;
+};
+
 class Tokenizer
 {
  public:

8b32cde17ab20522399ee1d38b6bae93981003ea
Wankai Zhang <wankaizhang@gmail.com>
2017-02-13 18:52:54 +0800

    Update tokenizer.cc

--- tokenizer.cc
+++ tokenizer.cc
@@ -7,8 +7,24 @@ Tokenizer::Tokenizer(const std::string& content)
 
 Token Tokenizer::Next()
 {
+  if (offset_ >= content_.length()) {
+    return kEOFToken;
+  }
+  
   int c = content_[offset_];
   
+  if (c == '\n') {
+    line_++;
+    column_ = 1;
+    
+    if (!isCompleted(last_token()) {
+      installError();
+      return kInvalidToken;
+    }
+  }
+        
   if (c == '<') {
+    if (status_ = kStatus) {
+    }
   }
 }

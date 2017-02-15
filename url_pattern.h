1b60decce5085cb601088158c9133aa77f7b28e5
Wankai Zhang <wankaizhang@gmail.com>
2017-02-15 21:14:26 +0800

    Update url_pattern.h

--- url_pattern.h
+++ url_pattern.h
@@ -5,8 +5,9 @@
 class UrlPattern
 {
  public:
-   
+  static Build(const std::string& s);
+  bool Match(std::unordered_map<string, string>* match_result);
  private:
   std::regex path_regex_;
-  std::unordered_map<std::string, std::regex> query_regex_;
+  std::unordered_map<std::string, std::regex> query_regexs_;
 };

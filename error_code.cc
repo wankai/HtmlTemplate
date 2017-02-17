3bde3cc550ee9e8e7cf8325baec756f12236234b
Wankai Zhang <wankaizhang@gmail.com>
2017-02-17 23:42:54 +0800

    Update error_code.cc

--- error_code.cc
+++ error_code.cc
@@ -3,3 +3,12 @@
 bool ErrorCode::ok() {
   return msg_ == NULL;
 }
+
+bool ErrorCode::code() {
+  int* p = reinterpret_cast<int*>(msg_);
+  return *p;
+}
+
+const char* ErrorCode::reason() {
+  return msg_ + 4;
+}

4b6dddadee411a194b062877e6b352e7e4e7d4ef
Wankai Zhang <wankaizhang@gmail.com>
2017-02-19 17:48:08 +0800

    Update xpath.h

--- xpath.h
+++ xpath.h
@@ -1,1 +1,25 @@
+#pragma once
 
+class XPath
+{
+ public:
+  class Node
+  {
+  };
+  
+  static bool Build(const std::string& format,
+                    XPath* xpath,
+                    std::string* error);
+  
+  typedef std::vector<XPath::Node>::iterator iterator;
+  typedef std::vector<XPath::Node>::const_iterator const_iterator;
+  
+  iterator begin();
+  const_iterator begin();
+  
+  iterator end();
+  const_iterator end();
+  
+ private:
+  std::vector<XPath::Node> nodes_;
+};
